db5fae91776c89d667f72e78c5a90982d506bfdf
DarkJans19 <jansito4221@gmail.com>
2026-02-04 07:48:22 -0500

    Implementacion de repositorio generico

--- PosSystem/src/main/java/com/ejemplo/repositorios/RepositorioGenerico.java
+++ PosSystem/src/main/java/com/ejemplo/repositorios/RepositorioGenerico.java
@@ -1,13 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.ejemplo.repositorios;
 
-/**
- *
- * @author jansito
- */
-public class RepositorioGenerico {
-    
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+
+public class RepositorioGenerico <T>{
+    private final Class<T> tipoEntidad;
+    private final EntityManager entityManager;
+
+    public RepositorioGenerico(EntityManager entityManager, Class <T> tipoEntidad){
+        this.tipoEntidad = tipoEntidad;
+        this.entityManager = entityManager;
+    }
+
+    public T crear(T entidad){
+        entityManager.persist(entidad);
+        entityManager.flush();
+        return entidad;
+    }
+
+    public Optional<T> buscarPorId(Integer id){
+        return Optional.of(entityManager.find(tipoEntidad, id));
+    }
+
+    public void eliminar(T entidad){
+        entityManager.remove(entidad);
+    }
+
+    public T actualizar(T entidad){
+        entityManager.merge(entidad);
+        return entidad;
+    }
 }
